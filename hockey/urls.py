3c1134c46c719062f71aad1800ea68027a07a867
Mike M <mikemaddem@gmail.com>
2020-01-05 22:33:46 -0500

    add base urls

--- hockey/urls.py
+++ hockey/urls.py
@@ -1,21 +1,24 @@
-"""hockey URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from paypal.standard.ipn import views as paypal_views
+
+from pages import views as pages_views
+
+
+handler404 = pages_views.notfound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', pages_views.index, name='index'),
+    path('about/', pages_views.about, name='about'),
+    path('terms/', pages_views.terms, name='terms'),
+    path('privacy/', pages_views.privacy, name='privacy'),
+    path('404/', pages_views.notfound),
+
+
 ]
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
